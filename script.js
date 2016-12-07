f1e0b0ea6536d44d1c9a3f43869f240173c2299a
Tammyslau29 <Tammyslau29@gmail.com>
2016-12-07 11:21:07 -0800

    CSS touchups

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $(".numeric_buttons").click(define_val);
     $(".arithmetic_buttons").click(define_val);
     $(".clear_buttons").click(define_val);
+    $(".equal_buttons").click(define_val);
 });
 // var my_calculator = new calculator(basic_math);
 // function basic_math(type, value, item) {
